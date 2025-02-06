dd4fd03fe82870da3cf49593ef8ed1f2dde1405b
AzeemRafique10 <urbancrime19@gmail.com>
2025-02-06 23:29:56 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PWA App</h1>
+      <p>This is a Progressive Web App (PWA).</p>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default App;
