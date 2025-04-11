36a668c0341e3f1702ddda77767ffc1173f82057
Azeemrafique10 <azeemrafique47@gmail.com>
2025-04-11 11:50:36 +0500

    add title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { useState } from "react";
 import ProductForm from "./forms/ProductForm";
 import DeleteForm from "./forms/DeleteForm";
 import AntButton from "./components/Buttons/AntButton";
+import { Typography } from "antd";
+
+const { Title } = Typography;
 
 const queryClient = new QueryClient();
 
@@ -18,7 +21,22 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="container">
-        <h1>Online & Offline Data Entry</h1>
+        <Title
+          level={1}
+          style={{
+            textAlign: "center",
+            background: "linear-gradient(90deg, #0f2027, #203a43, #2c5364)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            fontWeight: 700,
+            fontSize: "3rem",
+            marginBottom: "2rem",
+            letterSpacing: "1px",
+            textTransform: "uppercase",
+          }}
+        >
+          🖋️ Online & Offline Data Entry
+        </Title>
         <div style={{ display: "flex", gap: 10 }}>
           <AntButton onClick={() => setActiveForm("user")} label="User Form" />
           {/* <button onClick={() => setActiveForm("product")}>Product Form</button> */}
